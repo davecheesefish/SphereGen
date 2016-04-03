c7083c287c5a1a60968aa38b8e91849c175d891c
David Prior <david.prior@davecheesefish.com>
2016-04-03 12:12:18 +0100

    Changed base sphere shape from a tetrahedron to an icosahedron to reduce distortion of faces when it is refined.

--- SphereGen/Sphere.cs
+++ SphereGen/Sphere.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using SphereGen.Graphics;
@@ -30,28 +31,64 @@ namespace SphereGen
         /// </summary>
         private bool dirtyGeometry = false;
 
-        
+
         public Sphere()
         {
-            // Create a tetrahedron as the base shape
-            // Create the 4 vertices.
-            Vector3 vertex1 = new Vector3(-1, 1, -1);
-            Vector3 vertex2 = new Vector3(1, 1, 1);
-            Vector3 vertex3 = new Vector3(-1, -1, 1);
-            Vector3 vertex4 = new Vector3(1, -1, -1);
+            // Create an icosahedron as the base shape
+            // Create the 12 vertices.
+            // See https://en.wikipedia.org/wiki/Regular_icosahedron
+            float phi = (float)((1.0 + Math.Sqrt(5)) / 2.0); // Golden ratio
+            Console.WriteLine(phi);
+            Vector3[] vertices = new Vector3[12];
+            
+            // The "top" (not actually at the top due to rotation)
+            vertices[0] = new Vector3(0, phi, -1);
+            // Points surrounding the top, anticlockwise
+            vertices[1] = new Vector3(0, phi, 1);
+            vertices[2] = new Vector3(-phi, 1, 0);
+            vertices[3] = new Vector3(-1, 0, -phi);
+            vertices[4] = new Vector3(1, 0, -phi);
+            vertices[5] = new Vector3(phi, 1, 0);
+            // The "bottom"
+            vertices[6] = new Vector3(0, -phi, 1);
+            // Points surrounding the bottom, anticlockwise
+            vertices[7] = new Vector3(0, -phi, -1);
+            vertices[8] = new Vector3(-phi, -1, 0);
+            vertices[9] = new Vector3(-1, 0, phi);
+            vertices[10] = new Vector3(1, 0, phi);
+            vertices[11] = new Vector3(phi, -1, 0);
 
-            // Normalise the vertex vectors.
-            vertex1.Normalize();
-            vertex2.Normalize();
-            vertex3.Normalize();
-            vertex4.Normalize();
+            // Normalise the vertex vectors onto the unit sphere.
+            for (int i = 0; i < 12; ++i)
+            {
+                vertices[i].Normalize();
+            }
 
-            // Create the triangular faces.
+            // Create the 20 triangular faces.
             faces = new List<Triangle>();
-            faces.Add(new Triangle(vertex1, vertex3, vertex4));
-            faces.Add(new Triangle(vertex1, vertex4, vertex2));
-            faces.Add(new Triangle(vertex1, vertex2, vertex3));
-            faces.Add(new Triangle(vertex2, vertex4, vertex3));
+            // Faces around the top
+            faces.Add(new Triangle(vertices[0], vertices[1], vertices[2]));
+            faces.Add(new Triangle(vertices[0], vertices[2], vertices[3]));
+            faces.Add(new Triangle(vertices[0], vertices[3], vertices[4]));
+            faces.Add(new Triangle(vertices[0], vertices[4], vertices[5]));
+            faces.Add(new Triangle(vertices[0], vertices[5], vertices[1]));
+            // Faces around the bottom
+            faces.Add(new Triangle(vertices[6], vertices[7], vertices[8]));
+            faces.Add(new Triangle(vertices[6], vertices[8], vertices[9]));
+            faces.Add(new Triangle(vertices[6], vertices[9], vertices[10]));
+            faces.Add(new Triangle(vertices[6], vertices[10], vertices[11]));
+            faces.Add(new Triangle(vertices[6], vertices[11], vertices[7]));
+            // Faces around the middle
+            faces.Add(new Triangle(vertices[1], vertices[10], vertices[9]));
+            faces.Add(new Triangle(vertices[1], vertices[9], vertices[2]));
+            faces.Add(new Triangle(vertices[2], vertices[9], vertices[8]));
+            faces.Add(new Triangle(vertices[2], vertices[8], vertices[3]));
+            faces.Add(new Triangle(vertices[3], vertices[8], vertices[7]));
+            faces.Add(new Triangle(vertices[3], vertices[7], vertices[4]));
+            faces.Add(new Triangle(vertices[4], vertices[7], vertices[11]));
+            faces.Add(new Triangle(vertices[4], vertices[11], vertices[5]));
+            faces.Add(new Triangle(vertices[5], vertices[11], vertices[10]));
+            faces.Add(new Triangle(vertices[5], vertices[10], vertices[1]));
 
             dirtyGeometry = true;
         }
